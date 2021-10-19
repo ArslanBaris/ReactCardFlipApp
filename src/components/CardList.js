6a948e8c054d44f3fbbcdce4958d28bf89a4209d
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-19 19:15:45 +0300

    feat: CardList component added.

---
+++ src/components/CardList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// Components
+import Card from "./Card"
+
+const CardList = () => {
+
+    return(
+        <div>
+           <h2> Card list</h2>
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
+
+export default CardList;
