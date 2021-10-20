3571247f7d3ae67e758d567fffb572b1cf787cb2
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-20 14:58:46 +0300

    refactor: CardList component  revised.

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,18 +1,40 @@
 import React from "react";
+import { useContext, useState, useEffect } from "react";
+
+// Styles
+import './CardList.css'
 
 // Components
-import Card from "./Card"
+import Card from "./Card";
+
+// Contexts
+import { ChanelContext } from "../contexts/ChanelContext";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const CardList = () => {
-
-    return(
-        <div>
-           <h2> Card list</h2>
-            <Card />
-            <Card />
-            <Card />
+  const { chanels } = useContext(ChanelContext);
+  const { dark } = useContext(ThemeContext);
+  const theme = dark;
+  return (
+    <section
+      className="page-section"
+      style={{ background: theme.bg, color: theme.txt }}
+    >
+      <div className="container">
+        <div className="text-center">
+          <h2 className="section-heading text-uppercase">Chanels</h2>
+          <h3 className="section-subheading text-muted">
+            Chanels ratings
+          </h3>          
         </div>
-    )
-}
+        <div className="row">
+          {chanels.map((chanel) => {
+            return <Card chanel={chanel} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default CardList;
