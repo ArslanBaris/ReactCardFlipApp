2def28680dcc2597403ee811999ad8ce578b7a56
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-23 23:01:26 +0300

    refactor: CardFront.js revised.

--- src/components/CardFront.js
+++ src/components/CardFront.js
@@ -4,29 +4,33 @@ import { useContext, useState, useEffect } from "react";
 // Styles
 import "./Card.css";
 
-// Contexts
-import { ChanelContext } from "../contexts/ChanelContext";
-import { ThemeContext } from "../contexts/ThemeContext";
 
 const CardFront = ({ chanel }) => {
-  const { dark } = useContext(ThemeContext);
-  const theme = dark;
-  return (    
-      <div>       
-        <img className="card-img"  src={chanel.imageURL} alt="" />
-
+  return (
+    <div>
+      <div style={{height:"140px"}}>
+        <img  style={{height:chanel.height, marginTop:chanel.marginTop}}  src={chanel.imageURL} alt="" />
+        
+      </div>
+      <div style={{height:"30px"}}>
+          <hr  style={{width:"70%",margin:"auto"}}/>
+      </div>
+      
+      <div  style={{height:"60%"}}>
         <div className="portfolio-caption">
-          <div className="portfolio-caption-heading " >
-            {chanel.name}
+          <div className="portfolio-caption-heading ">{chanel.name}</div>
+          <div className="portfolio-caption-subheading " style={{color:"green",fontWeight:"bolder"}}>
+            {chanel.instantViewer}
           </div>
-          <div className="portfolio-caption-subheading text-muted" >
-            {chanel.x}
+          <div className="portfolio-caption-subheading" style={{color:"orange",fontWeight:"bolder"}}>
+            {chanel.estimateRating}
           </div>
-          <div className="portfolio-caption-subheading text-muted" >
-            {chanel.y}
-          </div>       
+          <div className="portfolio-caption-subheading"  style={{color:"yellow",fontWeight:"bolder"}} >
+            {chanel.estimateViewer}
           </div>
-</div> 
+        </div>
+      </div>
+    </div>
   );
 };
 
